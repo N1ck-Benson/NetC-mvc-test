d7905ce237ef1ac4a0fa23cd8f894c3369e93f87
Nick Benson <nk.benson@hotmail.com>
2021-06-01 16:13:06 +0100

    add a blogpost model with test attributes

---
+++ Nc.JuniorDeveloperExam/Models/BlogPost.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Nc.JuniorDeveloperExam.Models
+{
+    public class BlogPost
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+    }
+}
